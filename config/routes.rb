24f20fb53946bdb231a4c5ea312a85b4ec7cc279
tfuabio <114894082+tfuabio@users.noreply.github.com>
2023-01-08 17:35:12 +0000

    [add] 施設・ケアマネージャー別の認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :care_managers
-  devise_for :facilities
+  #devise_for :care_managers
+  #devise_for :facilities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ケアマネ認証用
+  devise_for :care_managers, controllers: {
+    registrations: "care_managers/registrations",
+    sessions: 'care_managers/sessions'
+  }
+
+  # 施設認証用
+  devise_for :facilities, controllers: {
+    registrations: "facilities/registrations",
+    sessions: "facilities/sessions"
+  }
 end
