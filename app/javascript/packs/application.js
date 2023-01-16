ac3e0c02ddfa24e7006cd919b9e9bcdaa88f1dfc
tfuabio <114894082+tfuabio@users.noreply.github.com>
2023-01-16 02:07:29 +0000

    [update] フォント変更：ロゴ、全体テキスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 import '../stylesheets/7-1-26.css'
+// import "4-1-4.js"
 
 Rails.start()
 Turbolinks.start()
