0f7d5b0991bbe6f03b663452025d38af43cca43f
tfuabio <114894082+tfuabio@users.noreply.github.com>
2023-01-16 00:17:07 +0000

    [update] ロゴ・ヘッダーデザイン

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
+import '../stylesheets/7-1-26.css'
 
 Rails.start()
 Turbolinks.start()
