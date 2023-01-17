dfaa1f77cc7a03341da443cab88d4159606b0dd7
tfuabio <114894082+tfuabio@users.noreply.github.com>
2023-01-17 16:13:07 +0000

    [update] 施設とケアマネ共通のログインページ ; [update] トップページレイアウト、下部にAbout等配置

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,8 @@ class HomesController < ApplicationController
   def top
   end
 
-  def about
+  def sign_in
+    @care_manager = CareManager.new
+    @facility = Facility.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'homes#top'
-  get '/about' => 'homes#about', as: 'about'
+  get '/sign_in' => 'homes#sign_in', as: 'sign_in'
 
   # ケアマネ認証用
   devise_for :care_manager, controllers: {
