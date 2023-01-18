e928b9ff935ed460e032b6f6364803fbcf7f3ba4
tfuabio <114894082+tfuabio@users.noreply.github.com>
2023-01-18 01:08:11 +0000

    [fix] ケアマネージャー側：deviseコントローラ

--- app/controllers/care_manager/confirmations_controller.rb
+++ app/controllers/care_manager/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CareManagers::ConfirmationsController < Devise::ConfirmationsController
+class CareManager::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
--- app/controllers/care_manager/passwords_controller.rb
+++ app/controllers/care_manager/passwords_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CareManagers::PasswordsController < Devise::PasswordsController
+class CareManager::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
   # def new
   #   super
