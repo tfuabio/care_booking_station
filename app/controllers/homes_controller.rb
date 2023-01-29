305ad0d4ae0c0413a46e2680f7c7b4a8a729004c
tfuabio <tfuabio@icloud.com>
2023-01-29 03:08:11 +0000

    [fix] トップページレイアウト

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,20 +1,25 @@
 class HomesController < ApplicationController
+  before_action :denying_authenticated_user
+
   def top
+  end
+
+  def sign_in
+    @care_manager = CareManager.new
+    @facility = Facility.new
+  end
+
+  def registration
+  end
+
+  private
+
+  # ログインしているユーザーを拒否する
+  def denying_authenticated_user
     if care_manager_signed_in?
       redirect_to care_manager_use_plans_path
     elsif facility_signed_in?
       redirect_to facility_booking_contacts_path
-    end
-  end
-
-  def sign_in
-    if care_manager_signed_in?
-      redirect_to care_manager_use_plans_path, alert: "ログイン中はログインページを表示できません。"
-    elsif facility_signed_in?
-      redirect_to facility_booking_contacts_path, alert: "ログイン中はログインページを表示できません。"
-    else
-      @care_manager = CareManager.new
-      @facility = Facility.new
     end
   end
 end
